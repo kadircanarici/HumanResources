300db1c6f0cd4db6d88872826109084538c5b7f8
kadircanarici <kadircanarc@gmail.com>
2023-02-20 20:37:12 +0300

    pdf görüntüleme denemeleri

---
+++ HumanResources.Web/Controllers/pdfController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResources.Web.Controllers
+{
+    public class pdfController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PdfViewer()
+        {
+            return View();
+        }
+
+    }
+}
