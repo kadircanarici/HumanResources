0e84b7284c37fc161ccba9461f49e5494d39276a
kadircanarici <kadircanarc@gmail.com>
2023-02-20 14:00:58 +0300

    login page ve controller tamamlandı

--- HumanResources.Web/Controllers/UserController.cs
+++ HumanResources.Web/Controllers/UserController.cs
@@ -51,7 +51,7 @@ namespace HumanResources.Web.Controllers
 
                 await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                return Json(user);
+                return RedirectToAction("index");
 
             }
             return View(appUser);
@@ -80,6 +80,57 @@ namespace HumanResources.Web.Controllers
         public IActionResult FindById(AppUser appUser)
         {
             return Json(unitOfWork.AppUser.GetFirstOrDefault(u => u.Id == appUser.Id));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetUserRoleNames()
+        {
+            return Json(unitOfWork.AppUserRole.GetAll().ToList());
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Edit(AppUser appUser)
+        {
+            AppUser foundUser = unitOfWork.AppUser.GetFirstOrDefault(x => x.Id == appUser.Id);
+
+            if (foundUser == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                foundUser.AppUserRoleId = appUser.AppUserRoleId;
+                foundUser.UserName = appUser.UserName;
+                foundUser.Name = appUser.Name;
+                foundUser.Email = appUser.Email;
+                foundUser.Phone = appUser.Phone.Replace("-", String.Empty);
+                foundUser.DateModified = DateTime.Now;
+                foundUser.IsActive = appUser.IsActive;
+                unitOfWork.AppUser.Update(foundUser);
+                unitOfWork.Save();
+
+
+                return Json(foundUser);
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Add(AppUser appUser)
+        {
+            AppUser foundUser = unitOfWork.AppUser.GetFirstOrDefault(x => x.UserName == appUser.UserName);
+            if(foundUser == null)
+            {
+                appUser.Password = "123456";
+                appUser.Phone = appUser.Phone.Replace("-", String.Empty);
+                unitOfWork.AppUser.Add(appUser);
+                unitOfWork.Save();
+                return Json(appUser);
+            }
+            else
+            {
+                return NotFound("Bu username'e sahip baska bir kullanıcı var");
+            }
+         
         }
     }
 }
--- HumanResources.Web/Program.cs
+++ HumanResources.Web/Program.cs
@@ -51,6 +51,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Login}/{id?}");
 
 app.Run();
